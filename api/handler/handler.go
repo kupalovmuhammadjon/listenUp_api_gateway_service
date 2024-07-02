dd0f572b79d062c2339b59bd7ecd451571d1a2d5
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-07-02 12:04:57 +0500

    added newHandler function

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	pbAuthentication "api_gateway/genproto/authentication"
+	pbCollaboration "api_gateway/genproto/collaborations"
+	pbComments "api_gateway/genproto/comments"
+	pbEpisodeMetadata "api_gateway/genproto/episode_metadata"
+	pbEpisodes "api_gateway/genproto/episodes"
+	pbPodcasts "api_gateway/genproto/podcasts"
+	pbUserManagement "api_gateway/genproto/user"
+	pbUserInteractions "api_gateway/genproto/user_interactions"
+)
+
+type Handler struct {
+	ClientAuthentication   pbAuthentication.AuthenticationClient
+	ClientCollaboration    pbCollaboration.CollaborationsClient
+	ClientComments         pbComments.CommentsClient
+	ClientEpisodeMetadata  pbEpisodeMetadata.EpisodeMetadataClient
+	ClientEpisodes         pbEpisodes.EpisodesServiceClient
+	ClientPodcasts         pbPodcasts.PodcastsClient
+	ClientUserManagement   pbUserManagement.UserManagementClient
+	ClientUserInteractions pbUserInteractions.UserInteractionsClient
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,8 @@
 package main
+
+import "api_gateway/api"
+
+func main() {
+	router := api.NewRouter()
+	router.Run()
+}
